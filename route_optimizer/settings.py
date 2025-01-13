cc8aff9fcb69dfac172a8ef2b9a4b5426c4c9908
Vahtabhai Chaudhari <76804522+vhc102@users.noreply.github.com>
2025-01-13 15:46:28 +0530

    Update settings.py

--- route_optimizer/settings.py
+++ route_optimizer/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-GOOGLE_MAPS_API_KEY = "AIzaSyB7In-YWGaj-4OZVvgr6DHnbzkPeOvsqdU"
+GOOGLE_MAPS_API_KEY = ""
